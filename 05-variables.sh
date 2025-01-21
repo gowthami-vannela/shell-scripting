474cae8d4ce532b7cbe8ebd23bc7f6404f8729d0
vannela <gowthamivannela9@gmail.com>
2025-01-21 15:44:46 +0530

    05-variables.sh

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-echo " please enter your username::"
+echo " please enter your username::" # text entered here will be added as value to variable
 
-read USERNAME
+read -s USERNAME # Here USERNAME is the variable name
+
+echo "username entered: $USERNAME"
+
+echo " please enter your password::"
+
+read -s PASSWORD
+
