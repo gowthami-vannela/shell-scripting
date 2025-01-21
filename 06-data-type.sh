379407a2caaf38f90a04275299c804778e48054c
vannela <gowthamivannela9@gmail.com>
2025-01-21 18:20:20 +0530

    add 06-data-type.sh

---
+++ 06-data-type.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+NUMBER1=$1
+NUMBER2=$2
+
+TIMESTAMP=$(date)
+echo "script executed at: $TIMESTAMP"
+
+SUM=$(($NUMBER1+$NUMBER2))
+
+echo "SUM of $NUMBER1 AND $NUMBER2 is: $SUM"
+
+
+
+
+
