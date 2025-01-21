b62cb9999123cb6c9a3c703ed44fa8f7387454e4
vannela <gowthamivannela9@gmail.com>
2025-01-21 14:52:59 +0530

    04-variables.sh

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=$1 # no space before and after equal
+Person2=$2
+
+echo "$Person1:: Hi $Person2"
+echo "$Person2:: Hello $Person1"
+echo "$Person1:: How are you:"
+echo "$Person2:: I am good. how are you?"
