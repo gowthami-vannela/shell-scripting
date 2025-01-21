f0a54d8dd04b5be18b208e3ac1e1861ac641d0b2
vannela <gowthamivannela9@gmail.com>
2025-01-21 14:19:14 +0530

    Added variables scripts

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,7 +3,9 @@
 Person1=Ramesh # no space before and after equal
 Person2=Suresh
 
-echo " $Person1:: Hi $Person2"
+echo "$Person1:: Hi $Person2"
 echo "$Person2:: Hello $Person1"
 echo "$Person1:: How are you:
 echo "$Person2:: I am good. how are you?"
+
+
